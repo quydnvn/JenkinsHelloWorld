98a89fbe0c48521aab1b51ddb12f33ed2fb75b25
quydnvn <quydnvn@gmail.com>
2024-11-23 23:52:07 +0700

    Update HelloWorld.java
    Edit this file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("---*** HelloWorld & Welcome to Jenkins Training ***---");
+        System.out.println("---*** HelloWorld & Welcome to Jenkins Training & Edit this file ***---");
     }
 }
