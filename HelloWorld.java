be287041aaf513cf1a2ec022cf9df84e45e60b5f
quydnvn <quydnvn@gmail.com>
2024-11-24 00:14:50 +0700

    Update HelloWorld.java
    Test change

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("---*** HelloWorld & Welcome to Jenkins Training & Edit this file ***---");
+        System.out.println("---*** HelloWorld & Welcome to Jenkins Training & Edit this file & Test it ***---");
     }
 }
